2a201fca665d4ab0c712c85d491f1e6b34289da4
AP Fritts <ap@apfritts.com>
2015-02-03 06:47:19 -0800

    Initial Commit

---
+++ Rotten Tomatoes/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Rotten Tomatoes
+//
+//  Created by AP Fritts on 2/3/15.
+//  Copyright (c) 2015 AP Fritts. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
